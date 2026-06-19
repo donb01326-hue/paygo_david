9805aee449d70125b7e840ae48918a62b14f0739
donb01326-hue <donb01326@gmail.com>
2026-06-19 19:30:35 +0100

    Update page.tsx

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -27,7 +27,7 @@ export default function SupportPage() {
 
   const handleTelegramSupport = () => {
     // Open Telegram support link in a new tab
-    const telegramUrl = "https://https://t.me/Gbravo12"
+    const telegramUrl = "https://t.me/pago_manager1"
     window.open(telegramUrl, "_blank")
   }
 
