234db91358a8d6dd62b89984ac147f565ac34c79
donb01326-hue <donb01326@gmail.com>
2026-08-12 16:37:29 +0100

    Update page.tsx

--- app/upgrade/payment/page.tsx
+++ app/upgrade/payment/page.tsx
@@ -32,7 +32,7 @@ export default function UpgradePaymentPage() {
   }
 
   const handleCopyAccountNumber = () => {
-    navigator.clipboard.writeText("6922211132")
+    navigator.clipboard.writeText("2089409330")
     setCopiedAccount(true)
     setTimeout(() => setCopiedAccount(false), 2000)
   }
@@ -95,7 +95,7 @@ export default function UpgradePaymentPage() {
                 <span>🔢</span> Account Number
               </p>
               <div className="flex items-center justify-between">
-                <p className="font-bold">6922211132</p>
+                <p className="font-bold">2089409330</p>
                 <button
                   onClick={handleCopyAccountNumber}
                   className="bg-orange-400 text-white px-3 py-1 rounded text-sm"
@@ -109,14 +109,14 @@ export default function UpgradePaymentPage() {
               <p className="text-gray-700 mb-1 text-sm flex items-center gap-1">
                 <span>🏦</span> Bank Name
               </p>
-              <p className="font-bold">MONIEPOINT MFB</p>
+              <p className="font-bold">KUDA MFB</p>
             </div>
 
             <div>
               <p className="text-gray-700 mb-1 text-sm flex items-center gap-1">
                 <span>🚹</span> Account Name
               </p>
-              <p className="font-bold">Paygo- AYO OMOWUMI TEWE</p>
+              <p className="font-bold">Paygo- ISAAC OLUWATOBI</p>
             </div>
           </div>
 
